798b26cae30a2e0166ec55e9d484b80eafa00e34
Fathi <babayejuadesina@gmail.com>
2022-07-26 15:14:32 +0100

    commercejs initialization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { commerce } from "./lib/commerce";
 import Cart from "./components/Cart/Cart";
-import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  console.log(products);
+
   return (
     <Router>
       <div className=" max-w-[1440px] m-auto">
