f769d5d32f4882e2a58ddca1d5146a02eac94b90
Fathi <babayejuadesina@gmail.com>
2022-08-26 23:08:12 +0100

    rendering cart icon conditionally

--- src/components/Cart/CartItems.jsx
+++ src/components/Cart/CartItems.jsx
@@ -14,15 +14,21 @@ const CartItems = ({ item }) => {
 
       <div className="p-2">
         <div className="flex justify-between flex-wrap items-center gap-2">
-          <span className="sm:text-lg">{item.name}</span>
+          <span className="sm:text-lg truncate max-w-[100px]" title={item.name}>
+            {item.name}
+          </span>
           <span>{item.line_total.formatted_with_symbol}</span>
         </div>
 
         <div className="flex item-center justify-between mt-4">
           <div>
-            <span className="mr-2">-</span>
+            <span className="mr-2 hover:bg-gray-200 cursor-pointer rounded-full p-2 w-5 h-5 inline-flex items-center justify-center">
+              -
+            </span>
             {item.quantity}
-            <span className="ml-2">+</span>
+            <span className="ml-2 hover:bg-gray-200 cursor-pointer rounded-full p-2 w-5 h-5 inline-flex items-center justify-center">
+              +
+            </span>
           </div>
           <button className="text-white text-xs uppercase bg-red-500 rounded p-2">
             remove
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = ({ totalCartItems }) => {
+  const location = useLocation();
+
   return (
     <header className="bg-white shadow-sm p-5 fixed top-0 left-0 right-0">
       <div className="flex items-center justify-between">
-        <h1>Logo</h1>
-        <Link to="/"></Link>
-        <Link to="/cart">
-          <div className="relative">
-            <FaShoppingCart className="text-xl" />
-
-            <span className="bg-red-500 h-4 w-4 rounded-full text-white flex items-center justify-center p-3 absolute top-[-12px] right-[-10px]">
-              {totalCartItems}
-            </span>
-          </div>
+        <Link to="/">
+          <h1>Logo</h1>
         </Link>
+        {location.pathname === "/" && (
+          <Link to="/cart">
+            <div className="relative">
+              <FaShoppingCart className="text-xl" />
+
+              <span className="bg-red-500 h-4 w-4 rounded-full text-white flex items-center justify-center p-3 absolute top-[-12px] right-[-10px]">
+                {totalCartItems}
+              </span>
+            </div>
+          </Link>
+        )}
       </div>
     </header>
   );
