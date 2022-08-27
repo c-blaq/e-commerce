431ddc67cd27a1c54f5c1d19e8a07c2f582370ab
Fathi <babayejuadesina@gmail.com>
2022-08-27 14:38:13 +0100

    refactor: Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import CartItems from "./CartItems";
 
 const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
@@ -7,13 +8,19 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
   return (
     <>
       <main className="mt-20">
+        <h1 className="text-xl w-11/12 sm:text-3xl m-auto sm:w-4/5 max-w-7xl  my-6">
+          Your Shopping Cart!
+        </h1>
         {!cart.line_items.length ? (
-          <p className="text-2xl m-4">No cart added!</p>
+          <p className=" text-xl sm:text-2xl m-4 mb-6  text-center lg:text-3xl">
+            You have no cart yet,{" "}
+            <Link to="/" className=" text-blue-500">
+              start adding
+            </Link>
+            !
+          </p>
         ) : (
           <div className="w-11/12 m-auto">
-            <h1 className="text-3xl m-auto sm:w-4/5 max-w-7xl  my-6">
-              Your Shopping Cart!
-            </h1>
             <div className="flex flex-wrap w-full m-auto item-center justify-center gap-4">
               {cart.line_items.map((item) => (
                 <CartItems
@@ -24,27 +31,28 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
                 />
               ))}
             </div>
+            <div className="flex gap-2 item-center w-11/12 flex-wrap sm:w-4/5 justify-between lg:w-4/6 m-auto mt-8">
+              <div>
+                <p className="text-xl lg:text-2xl">
+                  Subtotal: {cart.subtotal.formatted_with_symbol}
+                </p>
+              </div>
+              <div>
+                <button
+                  className="text-white text-xs uppercase bg-red-500 rounded p-2"
+                  onClick={onEmptyCart}
+                >
+                  Empty Cart
+                </button>
+                <Link to="/checkout">
+                  <button className="text-white ml-2 text-xs uppercase bg-blue-600 rounded p-2">
+                    Checkout
+                  </button>
+                </Link>
+              </div>
+            </div>
           </div>
         )}
-
-        <div className="flex gap-2 item-center w-11/12 flex-wrap sm:w-4/5 justify-between lg:w-4/6 m-auto mt-8">
-          <div>
-            <p className="text-xl lg:text-2xl">
-              Subtotal: {cart.subtotal.formatted_with_symbol}
-            </p>
-          </div>
-          <div>
-            <button
-              className="text-white text-xs uppercase bg-red-500 rounded p-2"
-              onClick={onEmptyCart}
-            >
-              Empty Cart
-            </button>
-            <button className="text-white ml-2 text-xs uppercase bg-blue-600 rounded p-2">
-              Checkout
-            </button>
-          </div>
-        </div>
       </main>
     </>
   );
