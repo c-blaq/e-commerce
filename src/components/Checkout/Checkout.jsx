7f8acb8e6bfb87e275109a9728a2b7565597fe83
Fathi <babayejuadesina@gmail.com>
2022-08-28 20:31:03 +0100

    refactor Checkout: stepper

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Checkout = () => {
   const steps = ["Shipping Address", "Payment details"];
-  const activeState = true;
+  const [activeStep, setActiveStep] = useState(0);
 
   return (
     <div className=" mt-16 flex flex-col items-center">
-      <div className="bg-white rounded shadow-sm w-2/3 p-4">
+      <div className="bg-white rounded shadow-sm w-2/3 p-4 max-w-3xl">
         <h2 className="text-center text-xl">Checkout</h2>
         <div className="flex gap-2 items-center">
-          {steps.map((step) => (
-            <button className="test" disabled={activeState}>
+          {steps.map((step, index) => (
+            <button className="test" disabled={index > activeStep}>
               {step}
             </button>
           ))}
